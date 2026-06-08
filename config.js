2b57d8d951d016f582c6ae787223ccdfaa6892aa
fst-kginvicta <kgfst15@gmail.com>
2026-06-09 00:20:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================================
 const CONFIG = {
   // ⚠️ REPLACE THIS WITH YOUR APPS SCRIPT WEB APP URL
-  API_URL: 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwZaTvdaFm0HaJTMcBSiX7hmGx7mfmnQPJbBSSEjzYNAUQoGbL3e01DQo6Ce3T22GZ6/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwZaTvdaFm0HaJTMcBSiX7hmGx7mfmnQPJbBSSEjzYNAUQoGbL3e01DQo6Ce3T22GZ6/exec',
   APP_NAME: 'FST - Field Support Technicians',
   VERSION: '1.0.0',
   DEFAULT_FOOD_EXPENSE: 500,
